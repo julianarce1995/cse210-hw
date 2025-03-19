ca966490b365b027ab460afa604e96c320cb8f24
Julian Arce <julianarce1101@gmail.com>
2025-03-18 22:30:22 -0500

    Week2 first actvity creating classes  with methods and instantiating it

---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,18 @@
+using System.Reflection.Metadata.Ecma335;
+
+public class Resume
+{
+    public String _name;
+    public List<Job> _jobs = new List<Job>();
+    public void Display()
+    {
+        Console.WriteLine("Name: " + _name);
+        Console.WriteLine("Jobs: ");
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+
+
+}
